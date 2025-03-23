880c9c09c90dc68ded2ff9ae9508bf50bf3b94b1
Jiachen ZHANG <jzhangea@connect.ust.hk>
2025-03-23 10:18:39 +0800

    refactor

--- products.py
+++ products.py
@@ -1,36 +1,54 @@
 import os # operating system
 
-products = []
-
-if os.path.isfile('products.csv'):
-	print('Yeah! We have found the file!')
-	with open('products.csv', 'r', encoding = 'utf-8') as f:
+def read_file(filename):
+	products = []
+	with open(filename, 'r', encoding = 'utf-8') as f:
 		for line in f:
 			if 'products,price' in line:
 				continue
 			name, price = line.strip().split(',')
 			products.append([name, price])
-	print(products)
+	return products
 
-else: 
-	print('Cannot find the file...')
+
 
 
 # let users input
-while True:
-	name = input('Please input the name of the products: ')
-	if name == 'q':
-		break
-	price = input('Please input the price of the products: ')
-	products.append([name, price])
-print(products)
+def user_input(products):
+	while True:
+		name = input('Please input the name of the products: ')
+		if name == 'q':
+			break
+		price = input('Please input the price of the products: ')
+		products.append([name, price])
+	print(products)
+	return products
 
 #print all the purchase records
-for p in products:
-	print('The price of', p[0], 'is', p[1])
+def print_products(products):
+	for p in products:
+		print('The price of', p[0], 'is', p[1])
 
 #write to the file
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-	f.write('products,price\n')
-	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+def write_file(filename, products):
+	with open(filename, 'w', encoding = 'utf-8') as f:
+		f.write('products,price\n')
+		for p in products:
+			f.write(p[0] + ',' + p[1] + '\n')
+
+
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename):
+		print('Yeah, we have found the file!')
+		products = read_file(filename)
+
+	else:
+		print('Cannot find the file...')
+
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)
+
+
+main()
